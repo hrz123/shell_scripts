154dd3158c473f19a6b338a3bb181e3d587a8e93
hrz123 <2403076194@qq.com>
2020-10-09 15:56:20 +0800

    New: while if and functions

---
+++ 13.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# help  display help help
+
+#for pos in $*
+#do
+#    if [ "$pos" = "help" ] ; then
+#        echo $pos $pos
+#    fi
+#done
+
+while [ $# -ge 1 ]
+do
+    if [ "$1" = "help" ] ; then
+        echo $1 $1
+        break
+    fi
+    shift
+done
